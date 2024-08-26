e5acac995e46a83f39096f10ad553de60bd6d002
Nassim HADDAD <nassim.haddad.pro@gmail.com>
2024-08-26 15:41:41 +0200

    Auth with clerk done (without database)

---
+++ lib/models/user.model.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true,
+    },
+    firstname: {
+        type: String,
+        required: true,
+    },
+    lastname: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
