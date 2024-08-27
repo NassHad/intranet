970f587e083348d419d1a900c706c5dd1b528980
Nassim HADDAD <nassim.haddad.pro@gmail.com>
2024-08-27 11:33:03 +0200

    Change sidebar to the flowbite react sidebar

--- components/layout/LeftSideBar.tsx
+++ components/layout/LeftSideBar.tsx
@@ -4,6 +4,16 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { SignOutButton, SignedIn, useAuth } from "@clerk/nextjs";
+import { Sidebar } from "flowbite-react";
+import {
+    HiArrowSmRight,
+    HiChartPie,
+    HiInbox,
+    HiShoppingBag,
+    HiTable,
+    HiUser,
+    HiViewBoards,
+} from "react-icons/hi";
 
 import { sidebarLinks } from "../../constants/index";
 
@@ -14,46 +24,39 @@ const LeftSidebar = () => {
     const { userId } = useAuth();
 
     return (
-        <aside className="flex w-64 duration-75 pt-16 flex-col">
-            <div className="pt-0 bg-white flex-col min-h-0 relative flex-1">
-                <div className="pb-4 pt-5 overflow-y-auto flex-col flex-1">
-                    <div className="pl-3 pr-3 bg-white flex-1">
-                        {sidebarLinks.map((link) => {
-                            // <li className="list-none space-y-reverse">
-                            //     <a
-                            //         className="font-normal text-base leading-6 p-2 items-center flex"
-                            //         href=""
-                            //     >
-                            //         {link.label}
-                            //     </a>
-                            // </li>
-                            const isActive =
-                                (pathname.includes(link.route) &&
-                                    link.route.length > 1) ||
-                                pathname === link.route;
-
-                            return (
-                                <Link
-                                    href={link.route}
-                                    key={link.label}
-                                    className={`font-normal text-base leading-6 p-2 items-center flex rounded-lg ${
-                                        isActive && "bg-slate-100"
-                                    }`}
-                                >
-                                    <Image
-                                        src={link.imgURL}
-                                        alt={link.label}
-                                        width={24}
-                                        height={24}
-                                    />
-
-                                    <span className="ml-3">{link.label}</span>
-                                </Link>
-                            );
-                        })}
-                    </div>
-                </div>
-            </div>
+        <aside className="flex w-64 duration-75 pt-16 flex-col transition-transform sm:translate-x-0 -translate-x-full h-full">
+            <Sidebar aria-label="Default sidebar example">
+                <Sidebar.Items>
+                    <Sidebar.ItemGroup>
+                        <Sidebar.Item href="#" icon={HiChartPie}>
+                            Dashboard
+                        </Sidebar.Item>
+                        <Sidebar.Item
+                            href="#"
+                            icon={HiViewBoards}
+                            label="Pro"
+                            labelColor="dark"
+                        >
+                            Kanban
+                        </Sidebar.Item>
+                        <Sidebar.Item href="#" icon={HiInbox} label="3">
+                            Inbox
+                        </Sidebar.Item>
+                        <Sidebar.Item href="#" icon={HiUser}>
+                            Users
+                        </Sidebar.Item>
+                        <Sidebar.Item href="#" icon={HiShoppingBag}>
+                            Products
+                        </Sidebar.Item>
+                        <Sidebar.Item href="#" icon={HiArrowSmRight}>
+                            Sign In
+                        </Sidebar.Item>
+                        <Sidebar.Item href="#" icon={HiTable}>
+                            Sign Up
+                        </Sidebar.Item>
+                    </Sidebar.ItemGroup>
+                </Sidebar.Items>
+            </Sidebar>
         </aside>
     );
 };
--- components/layout/TopBar.tsx
+++ components/layout/TopBar.tsx
@@ -8,7 +8,12 @@ export default function TopBar() {
             <div className="pl-3 pr-5 pb-3 pt-3">
                 <div className="flex justify-between items-center">
                     <div className="flex justify-start items-center">
-                        <button className="inline p-2 rounded cursor-pointer mr-3 ">
+                        <button
+                            className="inline p-2 rounded cursor-pointer mr-3"
+                            data-drawer-target="default-sidebar"
+                            data-drawer-toggle="default-sidebar"
+                            aria-controls="default-sidebar"
+                        >
                             <Image
                                 src="/assets/menu.svg"
                                 alt="logo"
