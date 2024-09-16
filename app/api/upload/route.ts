ed4d981a34bfb64b45e2ae6f950d50026a40385e
Nassim HADDAD <nassim.haddad.pro@gmail.com>
2024-09-16 10:09:05 +0200

    Change the upload folder (media_folder)

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -34,13 +34,7 @@ export async function POST(request: NextRequest) {
         const bytes = await file.arrayBuffer();
         const buffer = Buffer.from(bytes);
 
-        const uploadDir = path.join(
-            process.cwd(),
-            "..",
-            "media",
-            "public",
-            "document"
-        );
+        const uploadDir = path.join(process.cwd(), "..", "..", "media_folder");
         const safeName = path
             .basename(file.name)
             .replace(/[^a-zA-Z0-9.-]/g, "_");
